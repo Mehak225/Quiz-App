e6826484fb2bec614673258e7346519f5f6c7371
Mehak225 <mehakhanif225@gmail.com>
2024-01-18 16:23:04 -0800

    first commit

---
+++ public/question.js
@@ -0,0 +1,113 @@
+let questions =[
+    {
+        numb:1,
+        question: "What does HTML stand for ?",
+        answer: "Hyper Text Markup Language",
+        options:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question: "What does CSS stand for ?",
+        answer: "Cascading Style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    
+    {
+        numb:3,
+        question: "What does UI and UX stand for? ",
+        answer: "UserInterface and UserExperience",
+        options:[
+            "UserInterface and UserExperience",
+            "UserInterupt and UserEye",
+            "UserInface and UserEmphase",
+            "None",
+        ]
+    },
+    {
+        numb:4,
+        question: "Choose the correct option to select this image by its id?<img id=”mainpic”>",
+        answer: "#mainpic{ }",
+        options:[
+            "mainpic { }",
+            "img{ }",
+            "mainpic{ }",
+            "#mainpic{ }"
+        ]
+    },
+    {
+        numb:5,
+        question: "Select the appropriate HTML tag used for the largest heading?",
+        answer: "H1",
+        options:[
+            "Head",
+            "Heading",
+            "H1",
+            "H6"
+        ]
+    },
+    {
+        numb:6,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "alert('Hello World')",
+        options:[
+            "alertBox('Hello World')",
+            "msgBox('Hello World')",
+            "msg('Hello World')",
+            "alert('Hello World')"
+        ]
+    },
+    {
+        numb:7,
+        question: "Select the option to make a list that lists the items with numbers?",
+        answer: "OL",
+        options:[
+            "DL",
+            "OL",
+            "UL",
+            "None"
+        ]
+    },
+    {
+        numb:8,
+        question: "How do you create a function in JavaScript?",
+        answer: "function myFunction()",
+        options:[
+            "function myFunction()",
+            "function = myFunction()",
+            "function:myFunction()",
+            "None of the above"
+        ]
+    },
+    {
+        numb:9,
+        question: "What is the correct way to write a JavaScript array?",
+        answer:  "var colors = ['red','green','blue']",
+        options:[
+            "var colors = 'red','green','blue'",
+            "var colors = 1 = ('red'),2 = ('green'),3 = ('blue')",
+            "var colors = (1:'red',2:'green',3:'blue')",
+            "var colors = ['red','green','blue']"
+        ]
+    },
+    {
+        numb:10,
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answer:   "Math.round(7.25)",
+        options:[
+            "Math.round(7.25)",
+            "rnd(7.25)",
+            "round(7.25)",
+            "Math.rnd(7.25)"
+        ]
+    },
+]
